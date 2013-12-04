d4816931b6bee237fbd91eb42ec197ebd753f464
jonathan <jonathan@JHP-VirtualBox.(none)>
2013-12-04 13:24:11 +0000

    Añadido código de app.js

--- public/js/app.js
+++ public/js/app.js
@@ -5,3 +5,29 @@ $(function() {
   });
 });
 */
+$(document).ready(function(){
+  $(".cell").click(function(event){
+	  var pathname = window.location.pathname;
+		var ruta = pathname + event.target.id;
+		$.get(ruta, function(data){
+		  if(data == 'illegal'){
+			  alert("Illegal move!");
+				}
+			else if (data.length < 3){
+			  if(data.length > 0){
+				  $("#"+event.target.id).addClass("circle");
+					$("#"+data).addClass("cross");
+				}
+				else{
+				  alert("La casilla está ocupada, escoja otra");
+				}
+			}
+			else{
+			  setTimeout(function(){
+				url = data;
+				$(location).attr('href',url);
+				},200);
+			}
+		});
+	});
+});
